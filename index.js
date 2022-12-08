fbc070b8d03356dbfc54ef0559cc40a755d32ba9
wolako <benowola@gmail.com>
2022-12-08 18:45:49 +0000

    Version 1r

--- index.js
+++ index.js
@@ -33,4 +33,31 @@ function headerChgCol() {
   }
 }
 
+// nav link section
 window.addEventListener("scroll", headerChgCol);
+
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/* remove menu mobile */
+
+const navLink = document.querySelectorAll(".nav_link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
